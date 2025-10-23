5fe4030190bf6b11ff56b40ab5b1ada58de569f6
Matthew <matthewtindogan72@gmail.com>
2025-10-22 17:22:36 -0700

    changed the newline in part 1

--- lab_functions_arrays/functions_practice.php
+++ lab_functions_arrays/functions_practice.php
@@ -1,13 +1,13 @@
 <?php
 function greet(){
-	echo "Hello, World!<br>";
+	echo "Hello, World!\r\n";
 }
 
 greet();
 greet();
 
 function greetName($name){
-	echo "Hello, $name!<br>";
+	echo "Hello, $name!\r\n";
 }
 
 greetName("Alice");
@@ -17,16 +17,16 @@ function add($a, $b){
 	return $a + $b;
 }
 
-echo add(3,5) . "<br>";
-echo add(10,20) . "<br>";
+echo add(3,5) . "\r\n";
+echo add(10,20) . "\r\n";
 
 function convertCtoF($c){
 	return ($c * 1.8) + 32;
 }
 
-echo convertCtoF(0) . "<br>";
-echo convertCtoF(30) . "<br>";
-echo convertCtoF(100) . "<br>";
+echo convertCtoF(0) . "\r\n";
+echo convertCtoF(30) . "\r\n";
+echo convertCtoF(100) . "\r\n";
 
 function safeDivide($a, $b){
 	if($b === 0){
@@ -37,7 +37,7 @@ function safeDivide($a, $b){
 	}
 }
 
-echo safeDivide(10,2) . "<br>";
+echo safeDivide(10,2) . "\r\n";
 safeDivide(10,0);
 
 ?>
