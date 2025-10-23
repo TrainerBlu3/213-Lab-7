62af87fc4afb97178656347e0eb4fcfd2447013a
Shire-Bennett <stefanwells123@gmail.com>
2025-10-22 17:09:01 -0700

    lab 7 part 2

---
+++ lab_functions_arrays/lab_arrays_practice.php
@@ -0,0 +1,25 @@
+<?php
+$nums = array(11, 0, 75, 92, 8);
+
+$largestNum = max($nums);
+$smallestNum = min($nums);
+
+echo $largestNum;
+echo $smallestNum;
+
+$students = array("name"=>"Matt", "grade"=>49,
+"name"=>"Adam", "grade"=>80,
+"name"=>"Stefan", "grade"=>99);
+
+var_dump($students);
+
+$numsPart3 = array(1, 2, 3);
+$numsMerge = array(5, 6);
+array_push($numsPart3, 4);
+array_pop($numsPart3);
+array_merge($numsPart3, $numsMerge);
+
+$numsPart4 = array(90, 80, 90, 70, 80);
+array_count_values($numsPart4);
+
+
