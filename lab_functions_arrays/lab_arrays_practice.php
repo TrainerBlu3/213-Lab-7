3b6dbef29454cc61544a3fddcf4a87fd50ec268d
Shire-Bennett <stefanwells123@gmail.com>
2025-10-22 17:42:19 -0700

    yeh bub

--- lab_functions_arrays/lab_arrays_practice.php
+++ lab_functions_arrays/lab_arrays_practice.php
@@ -4,22 +4,27 @@ $nums = array(11, 0, 75, 92, 8);
 $largestNum = max($nums);
 $smallestNum = min($nums);
 
-echo $largestNum;
-echo $smallestNum;
+echo "Largest number: $largestNum\n";
+echo "Smallest number: $smallestNum\n";
 
-$students = array("name"=>"Matt", "grade"=>49,
-"name"=>"Adam", "grade"=>80,
-"name"=>"Stefan", "grade"=>99);
 
-var_dump($students);
+
+$students = array(
+    array("name"=>"Matt", "grade"=>49),
+    array("name"=>"Adam", "grade"=>80),
+    array("name"=>"Stefan", "grade"=>99)
+);
+
+print_r($students);
 
 $numsPart3 = array(1, 2, 3);
 $numsMerge = array(5, 6);
 array_push($numsPart3, 4);
 array_pop($numsPart3);
-array_merge($numsPart3, $numsMerge);
+$numsPart3 = array_merge($numsPart3, $numsMerge);
+print_r($numsPart3);
 
 $numsPart4 = array(90, 80, 90, 70, 80);
-array_count_values($numsPart4);
-
+$counts = array_count_values($numsPart4);
+print_r($counts);
 ?>
