ab6b4ad5b1545a4b82c61206ee98f3aed583c0fc
Matthew <matthewtindogan72@gmail.com>
2025-10-22 17:35:56 -0700

    finish part 3

--- lab_functions_arrays/combine_functions_arrays.php
+++ lab_functions_arrays/combine_functions_arrays.php
@@ -1,14 +1,43 @@
 <?php
-$grades =  array(66,77,88,99,50,100);
-
 function average($grades){
 	$total = 0;
 	$count = 0;
-	foreach($item as $grades){
+	foreach($grades as $item){
 		$total += $item;
 		$count ++;
 	}
-	return "Average is " . ($total/$count);
+	return ($total/$count) . "\r\n";
 }
 
+function letterGrade($scores){
+    if($scores >=80){
+        return "A\r\n";
+    }else if($scores >=70){
+        return "B\r\n";
+    }else if($scores >=60){
+        return "C\r\n";
+    }else if($scores >=50){
+        return "D\r\n";
+    }else{
+        return "F\r\n";
+    }
+}
+
+$studentGrades = array(
+    "Alice" => 80,
+    "Bob" => 90,
+    "Charlie" => 50,
+    "Dylan" => 67,
+    "Edward" => 39
+);
+
+$grades = array_values($studentGrades);
+$classAverage = average($grades);
+echo "Class Average is " . $classAverage;
+
+foreach($studentGrades as $name => $score){
+    echo "$name scored $score -> Grade: " . letterGrade($score);
+}
+
+?>
 
