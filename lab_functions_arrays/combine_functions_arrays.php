e3789ead884fb6112e3e71a6d39b8046c84d1f57
TrainerBlu3 <matthewtindogan72@gmail.com>
2025-10-22 17:15:05 -0700

    added Part 3

---
+++ lab_functions_arrays/combine_functions_arrays.php
@@ -0,0 +1,14 @@
+<?php
+$grades =  array(66,77,88,99,50,100);
+
+function average($grades){
+	$total = 0;
+	$count = 0;
+	foreach($item as $grades){
+		$total += $item;
+		$count ++;
+	}
+	return "Average is " . ($total/$count);
+}
+
+
